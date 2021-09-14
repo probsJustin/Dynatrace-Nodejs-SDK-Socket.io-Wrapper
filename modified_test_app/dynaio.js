33edcfbbf52311dc3da21b84d9a58eb5cf9f0c7c
Justin Hagerty <wheelsmanx@gmail.com>
2021-09-14 15:44:30 -0400

    attempted to add a .on listener for the socketio lib

--- modified_test_app/dynaio.js
+++ modified_test_app/dynaio.js
@@ -29,14 +29,36 @@ module.exports = class dynaio {
 		  error: (msg) => console.error("Dynaio SDK error: " + msg)
 		});
 		
+
+	}
+	
+	
+	traceIncomingMessage(dtTag, func_functionToDoCallBack) {
+	  // create a tracer instance and start the trace, ensure dynatraceTag is set
+	  const startData = Object.assign({ dynatraceTag: dtTag}, this.systemInfo);
+	  const tracer = this.dyna_sdk_api.traceIncomingMessage(startData);
+	  tracer.start(function processMessage(done) {
+			func_functionToDoCallBack(); 
+		process.nextTick(done);
+	  }, function onDone(err) {
+		if (err) {
+		  tracer.error();
+		}
+		tracer.end();
+	  });
+	}
+	
+	
+	on(func_event, func_callback){
 		//Receive dynatrace_connection_socket event and process event message
 		io.on('x-dynatrace_connection_socket', function(client){
 			client.on('x-dynatrace_connection_socket_message', function(data) {
-			});
+				return io.on(func_event, traceIncomingMessage(data['dtTag'], func_callback)); 
+			}			
 		}); 
-	}
-	
-	sendDynatraceRequest(func_data_message_to_send){
+		
+	}		
+	sendDynatraceRequest(func_event, func_data_message_to_send){
 		var dt_tag; 
 		const tracer_instance = this.dyna_sdk_api.traceOutgoingMessage(systemInfo);
 			tracer_instance.start(function sendTaggedMessage(){
@@ -63,6 +85,10 @@ module.exports = class dynaio {
 			key:"x-dynatrace-socketio-message",
 			value:func_data_message_to_send
 		})
+		dataDict.push({
+			key:"x-dynatrace-socketio-event", 
+			value:func_event
+		})
 		
 		//Send dynatrace_connection_socket event with dt tag event message
 		socket.on('x-dynatrace_connection_socket', msg => {
@@ -71,7 +97,7 @@ module.exports = class dynaio {
 	}
 	
 	emit(func_event, func_data_message_to_send){
-		sendDynatraceRequest(func_data_message_to_send);
+		sendDynatraceRequest(func_event, func_data_message_to_send);
 		return this.io.emit(func_event, func_data_message_to_send);
 	}
 	
